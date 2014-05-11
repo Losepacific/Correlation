f1c75c6dfcfaaa1d48859fb1fe22281d07901aa9
Losepacific <losepacific@gmail.com>
2014-05-11 22:08:32 +0800

    把資料處理步驟整理成bash的Script

--- transformer.sh
+++ transformer.sh
@@ -1,4 +1,28 @@
-awk '/^<..*68px..*>/||/<..*300px..*>/{print $0}' ~/下載/DEMOCRACY_TABLE_2007_v3-1.html > data_democracy.txt 
-awk -F'[<>]' '{print $3}' data_democracy.txt > data_democracy2.txt 
-sed 's/ [0-9=][0-9=]*&#.*;$//g ; s/&#.*;//g' data_democracy2.txt  > data_democracy.txt 
-awk 'BEGIN{a=0;b=0}{if(NR%2){national[a]=bash;a=a+1}else{democracy[b]=bash;b=b+1}}END{for(i=0;i<(NR/2);i=i+1){print national[i],democracy[i]}}' data_democracy.txt > data_democracy2.txt 
+#!/bin/bash
+Tmpfile1="${1}$(date +%y%m%d%H%M%S).txt"
+Tmpfile2=${Tmpfile1}"2"
+awk '
+/^<..*68px..*>/||/<..*300px..*>/{print $0}
+' $1 > $Tmpfile1 
+awk -F"[<>]" '
+{print $3}
+' $Tmpfile1 > $Tmpfile2 
+sed '
+s/ [0-9=][0-9=]*&#.*;$//g 
+s/&#.*;//g
+' $Tmpfile2  > $Tmpfile1
+#awk '
+#BEGIN{a=0;b=0}
+#{
+#	if(NR%2){
+#		national[a]=bash;a=a+1
+#	}else{
+#		democracy[b]=bash;b=b+1
+#	}
+#}
+#END{
+#	for(i=0;i<(NR/2);i=i+1){
+#		print national[i],":",democracy[i]
+#	}
+#}
+#' $Tmpfile1 > $Tmpfile2
