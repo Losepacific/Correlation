2211f78b0abaa9ca7c95c6ab2a4818c0b76ec8e9
Losepacific <Losepacific@gmail.com>
2014-05-21 01:24:16 +0800

    加上資料

--- Combine.sh
+++ Combine.sh
@@ -1,2 +1,2 @@
-  awk -F"\t" '{print $1,":",$9}' GDP.txt | sed 's/^  *//;s/  *\(:\)  */\1/;s/,//g' > GDP2.txt 
-  awk -F":" 'FNR == NR {national[$1] = $2 ;next}{printf "%s,%s\n",$0,national[$1]}' GDP2.txt 1_1.txt | cat -n
+#!/bin/bash
+awk -F":" 'FNR == NR {national[$1] = $2 ;next}{printf "%s,%s\n",$0,national[$1]}' $1 $2
